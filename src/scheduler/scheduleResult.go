58f12282a10c1c4d6b58425b659c6a22c6ca41fe
gliffcheung <gliffzhang@163.com>
2019-01-15 20:02:23 +0800

    [cluster]output scheduleResult.csv

--- src/scheduler/scheduleResult.go
+++ src/scheduler/scheduleResult.go
@@ -1,6 +1,8 @@
 package scheduler
 
 import (
+	"io/ioutil"
+	"strconv"
 	"types"
 
 	"github.com/golang/glog"
@@ -19,7 +21,21 @@ func init() {
 }
 
 func HandleExecuteResult() {
+	filename := "../scheduleResult.csv"
+	var content string
 	for result := range executeResultQ {
-		glog.Warning(result)
+		stamp := podInfo[result.Name].CreationTimestamp
+		createTime := strconv.FormatInt(stamp.ProtoTime().Seconds, 10)
+		startTime := strconv.FormatInt(result.StartTime, 10)
+		content += podInfo[result.Name].Namespace + "," + result.Name + "," + createTime + "," + startTime + "\n"
+	}
+	WriteWithIoutil(filename, content)
+}
+
+func WriteWithIoutil(filename, content string) {
+	data := []byte(content)
+	err := ioutil.WriteFile(filename, data, 0644)
+	if err != nil {
+		glog.Error()
 	}
 }
